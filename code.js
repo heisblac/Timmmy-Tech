8930695155db848a0f920326fcf4538221eb8895
Timmmy Tech <timoyinventures@gmail.com>
2025-09-28 13:31:03 +0100

    Add files via upload

---
+++ code.js
@@ -0,0 +1,6 @@
+var typed= new Typed('.dynamic-text', {
+            strings: ['Wordpress Developer', 'UI/UX Designer', 'Digital Marketer', 'Freelancer', 'IoT Expert', 'AI Expert', 'SEO Expert'],
+            typeSpeed: 50,
+            backSpeed: 50,
+            loop:true
+        })
